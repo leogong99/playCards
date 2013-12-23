6a7082069b4ba740e0de7679ecaeeee2c87b191b
Jing Gong <leogong99@gmail.com>
2013-12-23 08:38:00 -0800

    display player cards1

--- assets/www/js/gameEngine.js
+++ assets/www/js/gameEngine.js
@@ -6,9 +6,11 @@ lg.gameEngine = new function(){
 			playDeckObj.init(1, true);
 			playDeckObj.shuffleCard();
 			var cards = playDeckObj.getDeckCard();
+			var cardStr = '';
 			for(var i = 0; i < 13; i++){
-
+				cardStr += '<img id="cards' + i +'" class="cardsInHand" src="./img/cards/'+ (cards[i].rank + 1) + '_of_' + cards[i].color + '.png"/>';
 			}
+			$('#playerContent').html(cardStr);
 		}
 		
 	}
